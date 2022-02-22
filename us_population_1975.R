4f090daa63add884aaa1665456f27c986b9c19d4
elisavillar <maigusofi@gmail.com>
2022-02-22 14:01:57 -0500

    Upload

---
+++ us_population_1975.R
@@ -0,0 +1,18 @@
+# Create data frame
+state_pop <- data.frame(State = state.abb, Pop = as.vector(state.x77[,1]))
+# Create hover text
+state_pop$hover <- with(state_pop, paste(State, '<br>', "Population:", Pop))
+# Make state borders white
+borders <- list(color = toRGB("black"))
+# Set up some mapping options
+map_options <- list(
+  scope = 'usa',
+  projection = list(type = 'albers usa'),
+  showlakes = TRUE,
+  lakecolor = toRGB('lightblue')
+)
+plot_ly(z = ~state_pop$Pop, text = ~state_pop$hover, locations = ~state_pop$State,
+         type = 'choropleth', locationmode = 'USA-states',
+         color = state_pop$Pop, colors = 'Oranges', marker = list(line = borders)) %>%
+  layout(title = 'US Population in 1975', geo = map_options)
+
